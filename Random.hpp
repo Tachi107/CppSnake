eef1b135fb93314c01e9dd5d927a35687498df6c
Andrea <andreapappacoda02@gmail.com>
2020-03-28 23:06:05 +0100

    Added debug options (oops, wrong commit messages)

--- Random.hpp
+++ Random.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <random>
 #include <functional>
 #include <algorithm>
@@ -8,28 +9,32 @@
 #include <array>
 
 // https://codereview.stackexchange.com/questions/109260/seed-stdmt19937-from-stdrandom-device
-class Random {
-private:
-public:
+namespace Random {
+    namespace {
+        std::mt19937 randomGenerator;
+        bool initialized = false;
+
+        void init() {
+            std::array<unsigned, std::mt19937::state_size> random_data;
+            std::random_device source;
+            std::generate(random_data.begin(), random_data.end(), std::ref(source));
+            std::seed_seq seeds(random_data.begin(), random_data.end());
+            randomGenerator.seed(seeds);
+            initialized = true;
+        }
+    }
+
     template <typename type>
-    static type getInt(type low, type high) {
-        std::array<unsigned, std::mt19937::state_size> random_data;
-        std::random_device source;
-        std::generate(random_data.begin(), random_data.end(), std::ref(source));
-        std::seed_seq semi(random_data.begin(), random_data.end());
-        std::mt19937 randomGenerator(semi);
+    type getInt(type low, type high) {
+        if (!initialized) init();
         std::uniform_int_distribution<type> distribution(low, high);
         return distribution(randomGenerator);
     }
 
     template <typename type>
-    static type getReal(type low, type high) {
-        unsigned random_data[std::mt19937::state_size];
-        std::random_device source;
-        std::generate(std::begin(random_data), std::end(random_data), std::ref(source));
-        std::seed_seq semi(std::begin(random_data), std::end(random_data));
-        std::mt19937 randomGenerator(semi);
+    type getReal(type low, type high) {
+        if (!initialized) init();
         std::uniform_real_distribution<type> distribution(low, high);
         return distribution(randomGenerator);
     }
-};
+};  
